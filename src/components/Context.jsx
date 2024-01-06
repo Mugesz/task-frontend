81aeab61f02d5cc7a3b89c59298b2a084e8bceda
frekerzzzz <kannamugesh135@gmail.com>
2024-01-06 16:45:27 +0530

    done

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -19,7 +19,7 @@ export const DarkModeProvider = ({ children }) => {
 
   const fetchTasks = async () => {
     try {
-      const responce = await axios.get(`${config.Api}/task`);
+      const responce = await axios.get(`${config.Api}/tasks`);
       setTasks(responce.data);
     } catch (error) {
       console.log(error);
